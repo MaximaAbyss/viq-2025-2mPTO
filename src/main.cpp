ffe8da07f7a5c92c5b70f530b47d0877f16ee7aa
Mahin <famty001@gmail.com>
2026-03-17 17:00:04 +1300

    initital

---
+++ src/main.cpp
@@ -0,0 +1,196 @@
+#include "vex.h"
+
+using namespace vex;
+
+vex::brain       Brain;
+
+//      DRIVETRAIN  
+vex::motor dt_tl(PORT11, 1.0,true);
+vex::motor dt_tr(PORT5, 1.0,false);
+motor dt_bl(PORT1, 1.0,false);
+motor dt_br(PORT7, 1.0,true);
+
+motor_group dt_l(dt_tl, dt_bl);
+motor_group dt_r(dt_tr, dt_br);
+
+
+//      PTO
+motor pto_l(PORT10, 1.0, true);
+motor pto_r(PORT4, 1.0, false);
+
+//      PNEUMATICS
+pneumatic l_claw_finger(PORT12);
+pneumatic catch_r_claw(PORT6);
+pneumatic ninety_one(PORT2);
+
+//      OTHER
+vex::controller controller1;
+
+//      SETUP
+void setup(){
+    dt_l.setStopping(coast);
+    dt_r.setStopping(coast);
+
+    pto_l.setStopping(hold);
+    pto_l.setStopping(hold);
+
+    l_claw_finger.extend(cylinder1);
+    l_claw_finger.retract(cylinder2);
+    catch_r_claw.extend(cylinder1);
+    catch_r_claw.extend(cylinder2);
+    ninety_one.retract(cylinder2);
+}
+
+
+//      VARIABLES
+bool is_dt_stopped;
+bool is_l_claw_grabbing = false;
+bool is_r_claw_grabbing = false;
+bool is_finger_down = false;
+bool is_ninety_one_down = true;
+bool is_catch_extended = true;
+
+
+//      FUNCTIONS
+void manual_drive(int x_pos, int y_pos){
+    int left_dt_speed = y_pos + x_pos;
+    int right_dt_speed = y_pos - x_pos;
+
+    if(abs(left_dt_speed) >=5 || abs(right_dt_speed) >=5){
+        dt_tl.spin(forward);
+        dt_tr.spin(forward);
+        dt_bl.spin(forward);
+        dt_br.spin(forward);
+
+        dt_tl.setVelocity(left_dt_speed,pct);
+        dt_bl.setVelocity(right_dt_speed,pct);
+        dt_tr.setVelocity(right_dt_speed,pct);
+        dt_br.setVelocity(left_dt_speed,pct);
+        is_dt_stopped = false;
+    }
+    else if(is_dt_stopped == false){
+        dt_l.stop();
+        dt_r.stop();
+        is_dt_stopped = true;
+    }
+    else{
+        return;
+    }
+        
+}
+
+void move_6_bar(int controller_pos){
+    pto_l.spin(forward);
+    pto_r.spin(reverse);
+    pto_r.setVelocity(controller_pos,pct);
+    pto_l.setVelocity((-1) * controller_pos,pct);
+
+}
+
+void manual_arm(bool up_is_true){
+    pto_l.setVelocity(100,pct);
+    pto_r.setVelocity(100,pct);
+
+    if (up_is_true){
+        pto_l.spin(forward);
+        pto_r.spin(forward);
+    }
+    else{
+        pto_l.spin(reverse);
+        pto_r.spin(reverse);
+    }
+
+
+}
+
+void run_pto(){
+    
+
+
+    if(controller1.ButtonRDown.pressing()){
+        manual_arm(true);
+    }
+    else if(controller1.ButtonRUp.pressing()){
+        manual_arm(false);
+    }
+    else if(abs(controller1.AxisD.position()) > 5){
+        if(is_catch_extended){
+            catch_r_claw.retract(cylinder1);
+            is_catch_extended =! is_catch_extended;
+        }
+        move_6_bar(controller1.AxisD.position());
+        
+    }
+    else{
+        if(!is_catch_extended){
+            catch_r_claw.extend(cylinder1);
+            is_catch_extended =! is_catch_extended;
+        }
+        pto_l.stop();
+        pto_r.stop();
+        
+    }
+}
+
+
+void l_claw_move(){
+    is_l_claw_grabbing ? l_claw_finger.extend(cylinder1) : l_claw_finger.retract(cylinder1);
+    is_l_claw_grabbing =! is_l_claw_grabbing;
+}
+
+void r_claw_move(){
+    is_r_claw_grabbing ? catch_r_claw.extend(cylinder2) : catch_r_claw.retract(cylinder2);
+    is_r_claw_grabbing =! is_r_claw_grabbing;
+}
+
+void finger_move(){
+    is_finger_down ? l_claw_finger.retract(cylinder2) : l_claw_finger.extend(cylinder2);
+    is_finger_down = !is_finger_down;
+}
+
+void ninety_one_flip(){
+    is_ninety_one_down ? ninety_one.extend(cylinder2) : ninety_one.retract(cylinder2);
+    is_ninety_one_down =! is_ninety_one_down;
+}
+
+void load_beam(){
+    pto_l.setVelocity(100,pct);
+    pto_r.setVelocity(100,pct);
+    pto_l.spinToPosition(1.34,turns,false);
+    pto_r.spinToPosition(-1.34,turns,true);
+    wait(0.5,sec);
+
+    l_claw_finger.extend(cylinder1);
+    catch_r_claw.extend(cylinder2);
+
+    wait(0.5, sec);
+    pto_l.spinToPosition(0,turns,false);
+    pto_r.spinToPosition(0,turns,true);
+
+}
+
+
+
+void setup_callbacks(){
+    controller1.ButtonFDown.pressed(l_claw_move);
+    controller1.ButtonFUp.pressed(r_claw_move);
+    controller1.ButtonEDown.pressed(ninety_one_flip);
+    controller1.ButtonR3.pressed(finger_move);
+
+    controller1.ButtonLDown.pressed(load_beam);
+
+    
+}
+
+
+int main() {
+	
+    setup();
+    setup_callbacks();
+   
+    while(1) {
+        manual_drive(controller1.AxisB.position(), controller1.AxisA.position());
+        run_pto();
+        this_thread::sleep_for(10);
+    }
+}
